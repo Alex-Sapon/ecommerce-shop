a0fd72d5b864e9ffe6814b6f950115b91f858756
Aleksandr_Saponchik <alexsapon@gmail.com>
2023-03-25 20:34:30 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { App } from 'components/app';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      assets: '/src/assets',
+      components: '/src/components',
+      pages: '/src/pages',
+      contexts: '/src/contexts',
+      api: '/src/api',
+    },
+  },
+});
